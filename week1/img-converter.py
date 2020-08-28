d4f3449969744559b8a8e1dd75f0aca1596ad26e
Euhid Aman <aman.derax20@gmail.com>
2020-08-28 22:02:54 +0530

    modified img-converter.py to complete week1 task

--- week1/img-converter.py
+++ week1/img-converter.py
@@ -1,3 +1,7 @@
+# If you are using this to solve the "Automating Real-World Tasks with Python-Week 1" assessment.
+# then, write this file in the home directory, storing the sub directories "images" and "/opt/icons/".
+# to go to home directory, use this command ----> cd~
+
 import os
 from PIL import Image
 
@@ -7,9 +11,13 @@ old_path = os.path.expanduser('~') + '\mnt\c\Users\Dell\Desktop\googlepython\Goo
 new_path = os.path.expanduser('~') + '\mnt\c\Users\Dell\Desktop\googlepython\Google-automating-real-world-tasks-with-python\Week1-Scale-and-convert-images-using-PIL\image_new'
 # I saved the modified images in the sub-folder 'image_new', present in the main folder 'Week1-Scale-and-convert-images-using-PIL'
 
+#to solve the qwiklabs assesment, use the below lines, instead of the above lines ---->
+#old_path = os.path.expanduser('~') + '/images/'
+#new_path = '/opt/icons/'
+
 for image in os.listdir(old_path):
-    print(image)
-    if '.' not in image[0]:
+    if image.endswith("48dp"):
         img = Image.open(old_path + image)
-        img.rotate(-90).resize((128, 128)).convert("RGB").save(new_path + image.split('.')[0], 'jpeg')
+        img.rotate(-90).resize((128, 128))
+        img.convert("RGB").save(new_path + image.split('.')[0], 'jpeg')
         img.close()
