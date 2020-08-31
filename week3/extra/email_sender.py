7a25726dd025b6f8a74f3280b907343045336f60
Euhid Aman <aman.derax20@gmail.com>
2020-08-31 15:23:08 +0530

    Modified program to send only text messages

--- week3/extra/email_sender.py
+++ week3/extra/email_sender.py
@@ -17,27 +17,7 @@ body = input("Enter body of message : ")
 
 message.set_content(body)
 
-x = input("Do you want to add an attachment ? Y/N\n")
-
-if (x == "Y"):
-    attachment_path = input("Enter attachment path : \n")
-    attachment_filename = os.path.basename(attachment_path)
-
-    mime_type, _ = mimetypes.guess_type(attachment_path)
-    mime_type, mime_subtype = mime_type.split('\',1)
-
-    with open(attachment_path, 'rb') as ap:
-        message.add_attachment(ap.read(),
-                               maintype = mime_type,
-                               subtype = mime_subtype,
-                               filename = os.path.basename(attachment_path))
-    print("Sending message.....")
-
-else:
-    print("Sending message.....")
-
 mail_server = smtplib.SMTP_SSL('smtp.gmail.com')
-mail_server.set_debuglevel(1)
 
 mail_pass = os.environ['EMAIL_PASS']
 
