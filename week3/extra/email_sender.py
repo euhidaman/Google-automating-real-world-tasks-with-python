387f2164a41745b136ace2cee124edd0d548e8da
Euhid Aman <aman.derax20@gmail.com>
2020-08-31 19:25:03 +0530

    modified the script to take all user credentials

--- week3/extra/email_sender.py
+++ week3/extra/email_sender.py
@@ -1,11 +1,11 @@
 from email.message import EmailMessage
-import os
+import getpass
 import smtplib
 import mimetypes
 
 message = EmailMessage()
 
-sender = "aman.derax20@gmail.com"
+sender = input("Enter you email address :")
 reciever = input("Enter reciepient's email address : ")
 subject = input("Enter subject of message : ")
 
@@ -19,10 +19,13 @@ message.set_content(body)
 
 mail_server = smtplib.SMTP_SSL('smtp.gmail.com')
 
-mail_pass = os.environ['EMAIL_PASS']
+# Make an app password for accessing you email through SMTP
+# To know, how to do that for gmail, check out this link ----> https://support.google.com/accounts/answer/185833?hl=en
+mail_pass = getpass.getpass('Enter SMTP gmail Password : ')
+# the input for the above line will be blank
 
 try:
-    mail_server.login("aman.derax20@gmail.com", mail_pass)
+    mail_server.login(sender, mail_pass)
     print("login successful !!")
     mail_server.send_message(message)
     print("Message sent successfully !!!")
